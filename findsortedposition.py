e8e14622b74cc23f4f9552b6c991ceee6fd4dad2
raghav-1990 <49017856+raghav-1990@users.noreply.github.com>
2020-08-14 14:58:11 +0530

    Create findsortedposition.py

---
+++ findsortedposition.py
@@ -0,0 +1,13 @@
+def findsortedposition(seq, target):
+    low = 0
+    high = len(seq)-1
+    while low <= high:
+        mid = (low + high)// 2
+        if seq[mid] == target:
+            print seq[mid]
+            return mid
+        elif target < seq[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return low
