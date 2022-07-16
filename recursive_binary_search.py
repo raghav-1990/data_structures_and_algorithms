dc669fd81d80f4c73eb73a3aa6c2bde372bf721f
raghav-1990 <49017856+raghav-1990@users.noreply.github.com>
2022-07-16 22:37:20 +0530

    Update recursive_binary_search.py

--- recursive_binary_search.py
+++ recursive_binary_search.py
@@ -6,10 +6,22 @@ def recursive_binary(array, target):
     if array[mid_point] ==  target:
         return True
     elif array[mid_point] > target:
-        return recursive_binary(array[:mid_point-1], target)
+        return recursive_binary(array[:mid_point], target)
     else:
         return recursive_binary(array[mid_point+1:], target)
 
+#another attempt
+def rec_binary_search(seq, target):
+    if len(seq) == 0:
+        return False
+    else:
+        mid = len(seq) //2
+        if seq[mid] == target:
+            return True
+        elif seq[mid] > target:
+            return rec_binary_search(seq[:mid], target)
+        else:
+            return rec_binary_search(seq[mid+1:], target)
 
 array = [1,2,3,4,5,6,7,8,9]
 print(recursive_binary(array, 12))
